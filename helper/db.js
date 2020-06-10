7d47f842216efca720b5748873ba1541f75eb564
Murat Denizli <muratdenizli@gmail.com>
2020-06-11 00:17:04 +0300

    connection string set edildi

--- helper/db.js
+++ helper/db.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 module.exports=()=>{
-    mongoose.connect('mongodb://localhost/movie-api',{useMongoClient:true});
+    
+    mongoose.connect('mongodb+srv://movie_user:abcd1234@homecarecluster-4itu1.mongodb.net/movie-api?retryWrites=true&w=majority');
     mongoose.connection.on('open',()=>{
         console.log('Mongo DB Connected');
     });
