267711c433ada7260953d208905e2a895658a23f
Murat Denizli <muratdenizli@gmail.com>
2020-06-16 15:29:55 +0300

    director validation setting

--- models/Director.js
+++ models/Director.js
@@ -1,15 +1,26 @@
 const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
+const Schema  = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name:String,
-    surname:String,
-    bio:String,
-    createAt:{
-        type:Date,
-        default:Date.now
-    }
+	name: {
+		type: String,
+		maxlength: 60,
+		minlength: 2
+	},
+	surname: {
+		type: String,
+		maxlength: 60,
+		minlength: 2
+	},
+	bio: {
+		type: String,
+		maxlenght: 1000,
+		minlength: 60
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
+	}
 });
 
-module.exports=mongoose.model('director',DirectorSchema);
+module.exports = mongoose.model('director', DirectorSchema);
