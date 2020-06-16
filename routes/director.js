06269a5df0d74ad500bd0dc7b869c877e82e5384
Murat Denizli <muratdenizli@gmail.com>
2020-06-16 15:07:25 +0300

    director details write endpoint

--- routes/director.js
+++ routes/director.js
@@ -1,17 +1,116 @@
+const mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
 
 const Director = require('../models/Director.js');
 
-router.post('/', function (req, res, next) {
-  const director = new Director(req.body);
+router.get('/', (req, res) => {
+	const promise = Director.aggregate([
+		{
+			$lookup: {
+				from: 'movies',
+				localField: '_id',
+				foreignField: 'director_id',
+				as: 'movies'
+			}
+		},
+		{
+			$unwind: {
+				path: '$movies',
+				preserveNullAndEmptyArrays: true
+			}
+		},
+		{
+			$group: {
+				_id: {
+					_id: '$_id',
+					name: '$name',
+					surname: '$surname',
+					bio: '$bio'
+				},
+				movies: {
+					$push: '$movies'
+				}
+			}
+		},
+		{
+			$project: {
+				_id: '$_id._id',
+				name: '$_id.name',
+				surname: '$_id.surname',
+				movies: '$movies'
+			}
+		}
+	]);
 
-  const promise = director.save();
-  promise.then((data) => {
-    res.json({ status: 1 });
-  }).catch((err) => {
-    res.json(err);
-  });
+	promise.then((data) => {
+		res.json(data);
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
+
+router.get('/:director_id', (req, res) => {
+	const promise = Director.aggregate([
+		{
+			$match: {
+				'_id': mongoose.Types.ObjectId(req.params.director_id)
+			}
+		},
+		{
+			$lookup: {
+				from: 'movies',
+				localField: '_id',
+				foreignField: 'director_id',
+				as: 'movies'
+			}
+		},
+		{
+			$unwind: {
+				path: '$movies',
+				preserveNullAndEmptyArrays: true
+			}
+		},
+		{
+			$group: {
+				_id: {
+					_id: '$_id',
+					name: '$name',
+					surname: '$surname',
+					bio: '$bio'
+				},
+				movies: {
+					$push: '$movies'
+				}
+			}
+		},
+		{
+			$project: {
+				_id: '$_id._id',
+				name: '$_id.name',
+				surname: '$_id.surname',
+				movies: '$movies'
+			}
+		}
+	]);
+
+	promise.then((data) => {
+		res.json(data);
+	}).catch((err) => {
+		res.json(err);
+	});
+});
+
+router.post('/', function (req, res, next) {
+    const director = new Director(req.body);
+
+    const promise = director.save();
+    promise.then((data) => {
+        res.json({ status: 1 });
+    }).catch((err) => {
+        res.json(err);
+    });
 });
 
 
